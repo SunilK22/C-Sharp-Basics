fde8ab843882b38e647a83c4a7f770f71adea000
Sunil Kamble <sunil@rcx.onmicrosoft.com>
2026-07-06 20:23:21 +0530

    Team Tracker project by implementing week 1 training

---
+++ Team Tracker/Person.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Team_Tracker
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int Experience { get; set; }
+
+
+        public Person(string name, string role, int experience)
+        {
+            Name = name;
+            Role = role;
+            Experience = experience;
+        }
+
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, working as a {Role} and I have {Experience} years of experience.");
+        }
+    }
+}
---
+++ Team Tracker/TeamTrackerProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Team_Tracker;
+
+
+class TeamTrackerProgram
+{
+    static void Main(string[] args)
+    {
+        List<Person> persons = new List<Person>();
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Enter your name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter your role: ");
+                string role = Console.ReadLine();
+                Console.WriteLine("Enter your experience in years: ");
+                int exp = Convert.ToInt32(Console.ReadLine());
+
+                Person p = new Person(name, role, exp);
+                persons.Add(p);
+
+                Console.WriteLine("Do you want to add more members? (yes/no)");
+
+                string isContinue = Console.ReadLine();
+
+                if (isContinue.ToLower() != "yes")
+                {
+                    break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input: got exception " + ex.Message);
+                Console.WriteLine("Please provide valid details");
+            }
+        }
+
+        foreach (var item in persons)
+        {
+            item.Introduce();
+        }
+
+    }
+}
